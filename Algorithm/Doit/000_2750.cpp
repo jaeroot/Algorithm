dfd49bee879b73131897ff663cc03ce875768145
jaeroot <jaeroot98@gmail.com>
2024-09-06 17:10:03 +0900

    2750

--- Algorithm/Doit/000_2750.cpp
+++ Algorithm/Doit/000_2750.cpp
@@ -1,8 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
 
 int main()
 {
-	std::cout << "123" << std::endl;
+	int N;
+	scanf("%d", &N);
+
+	vector<int> vec;
+	vec.reserve(N);
+
+	for (int i = 0; i < N; ++i)
+	{
+		int num;
+		scanf("%d", &num);
+		vec.push_back(num);
+	}
+
+	sort(vec.begin(), vec.end());
+
+	for (auto v : vec)
+	{
+		printf("%d\n", v);
+	}	
+
 
 	return 0;
 }
